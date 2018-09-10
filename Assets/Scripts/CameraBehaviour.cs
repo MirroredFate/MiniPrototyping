f4ab59d75f3f7566587029fc18827fd37ec72152
MirroredFate <MirroredFate@outlook.de>
2018-09-10 09:51:33 +0200

    Camera Behaviour

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    public Transform target;
+
+    [SerializeField] float heightOffset = 5f;
+    [SerializeField] float cameraDistance = 3f;
+
+    Vector3 cameraPosition;
+
+	// Use this for initialization
+	void Start () {
+        cameraPosition = new Vector3(target.position.x, target.position.y + heightOffset, target.position.z - cameraDistance);
+        transform.position = cameraPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = cameraPosition;
+	}
+}
