095271cb4e37959526a41e05a33a036e0fa6a70e
Edward Gommer <edward.gommer@BK.local>
2018-09-10 13:29:14 +0200

    ParticleSystem Prototype

---
+++ Assets/Particle/RainController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainController : MonoBehaviour {
+
+    ParticleSystem pS;
+    ParticleSystem.EmissionModule pSE;
+    
+
+
+    private void Awake()
+    {
+        pS = GetComponentInChildren<ParticleSystem>();
+        pS.Pause();
+        pSE = pS.emission;
+        
+    }
+
+    private void Update()
+    {
+   
+        if (Input.GetKey(KeyCode.R))
+        {
+            pSE.rateOverTime = 10;
+            pS.Play();
+        }
+        else
+        {
+            pSE.rateOverTime = 0;
+        }
+        
+
+        
+    }
+}
