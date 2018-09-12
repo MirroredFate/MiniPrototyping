10a9d7c3fd041b3ee8ea60cffebcdc0a81305596
Edward Gommer <edward.gommer@BK.local>
2018-09-12 15:36:38 +0200

    GrassBehaviour added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+   public Stack<GameObject> grassStack;
+    [SerializeField]GameObject grass;
+   public Transform tP;
+    int grassStackCount = 500;
+
+    private void Awake()
+    {
+        grassStack = new Stack<GameObject>();
+
+        for (int i = 1; i <= grassStackCount; i++)
+        {
+            GameObject newGrassQuad = Instantiate(grass, transform.position, Quaternion.Euler(Vector3.zero), tP);
+            newGrassQuad.SetActive(false);
+            grassStack.Push(newGrassQuad);
+
+        }
+        
+    }
+
+    public GameObject GetGrassQuad(Vector3 pos, Quaternion quat)
+    {
+        GameObject grass = grassStack.Pop();
+        grass.SetActive(true);
+        grass.transform.position = pos;
+        grass.transform.rotation = quat;
+        return grass;
+    } 
+
+    private void Update()
+    {
+      
+    }
+}
---
+++ Assets/Scripts/ParticleCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollision : MonoBehaviour {
+
+    ParticleSystem pS;
+    GameManager gM;
+  
+    List<ParticleCollisionEvent> collEvents;
+
+    private void Awake()
+    {
+        pS = GetComponent<ParticleSystem>();
+        gM = FindObjectOfType<GameManager>();
+
+        collEvents = new List<ParticleCollisionEvent>();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int numCollEvents = pS.GetCollisionEvents(other, collEvents);
+        
+        for (int b = 0; b < numCollEvents; b++)
+        {
+            Vector3 pos = collEvents[b].intersection;
+            gM.GetGrassQuad(pos, Quaternion.Euler(90f, 0f, 0f));
+        }
+
+    }
+}
